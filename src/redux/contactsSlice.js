7d3212cca5d7c1fec7fd5862323b27667e21fdb3
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-03-21 11:49:44 +0200

    contacts slice upg

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,10 +3,6 @@ import { fetchContacts, addContact, deleteContact } from './operations';
 
 const handlePending = state => {
   state.isLoading = true;
-};
-
-const handleFulfilled = state => {
-  state.isLoading = false;
   state.error = null;
 };
 
@@ -28,19 +24,19 @@ const contactsSlice = createSlice({
     builder
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.isLoading = false;
         state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, handleRejected)
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.isLoading = false;
         state.items.push(action.payload);
       })
       .addCase(addContact.rejected, handleRejected)
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.isLoading = false;
         const index = state.items.findIndex(
           item => item.id === action.payload.id
         );
@@ -56,19 +52,19 @@ const contactsSlice = createSlice({
 //   extraReducers: {
 //     [fetchContacts.pending]: handlePending,
 //     [fetchContacts.fulfilled](state, action) {
-//       handleFulfilled(state);
+//       state.isLoading = false;
 //       state.items = action.payload;
 //     },
 //     [fetchContacts.rejected]: handleRejected,
 //     [addContact.pending]: handlePending,
 //     [addContact.fulfilled](state, action) {
-//       handleFulfilled(state);
+//       state.isLoading = false;
 //       state.items.push(action.payload);
 //     },
 //     [addContact.rejected]: handleRejected,
 //     [deleteContact.pending]: handlePending,
 //     [deleteContact.fulfilled](state, action) {
-//       handleFulfilled(state);
+//      state.isLoading = false;
 //       const index = state.items.findIndex(
 //         item => item.id === action.payload.id
 //       );
